67bb62a36be65d0331978bab69900870e1349750
AsamK <asamk@gmx.de>
2022-01-05 18:22:07 +0100

    Fix upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -e
 npm run build
 npm run buildServer
-rsync -za lib img dist style package.json mwa:/opt/tarot/
+rsync -za lib img dist style package.json package-lock.json mwa:/opt/tarot/
 ssh mwa "cd /opt/tarot && npm install --production"
 ssh -t mwa "sudo systemctl stop tarot.service"
